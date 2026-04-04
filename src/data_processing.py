d18c6f57e0cca3fa929998899d958d91f4b44b1b
Jaime-r-h <jruizhuerta23@gmail.com>
2026-04-04 17:20:37 +0200

    primera versión de data_processing

--- src/data_processing.py
+++ src/data_processing.py
@@ -2,9 +2,11 @@ from typing import List, Tuple, Dict, Generator
 from src.utils import clean_text, tokenize, get_sentiment_label
 import csv
 from collections import Counter
+import numpy as np
 
-def load_and_preprocess_data(infile: str) -> List[Tuple[List[str], int]]:
-    processed_data = []
+def get_tokens_and_labels(infile: str):
+    tokens_list = []
+    labels_list = []
 
     with open(infile,"r", encoding="utf-8") as f:
         reader = csv.DictReader(f)
@@ -18,26 +20,52 @@ def load_and_preprocess_data(infile: str) -> List[Tuple[List[str], int]]:
 
             label = get_sentiment_label(sentiment)
 
-            processed_data.append((tokens, label))
+            tokens_list.append(tokens)
+            labels_list.append(label)
 
-    return processed_data
+    return tokens_list, labels_list
 
 
-def create_lookup_tables(words: List[str]) -> Tuple[Dict[str, int], Dict[int, str]]:
-    """
-    Create lookup tables for vocabulary.
+def create_lookup_tables(texts: List[List[str]], min_freq=1) -> Tuple[Dict[str, int], Dict[int, str]]:
 
-    Args:
-        words (List[str]): A list of words from which to create vocabulary.
+    special_tokens = ['<PAD>', '<UNK>', '<SOS>', '<EOS>']
+    
+    word_counts: Counter = Counter(tok for text in texts for tok in text)
 
-    Returns:
-        A tuple containing two dictionaries. The first dictionary maps words to integers (vocab_to_int),
-        and the second maps integers to words (int_to_vocab).
-    """
-    word_counts: Counter = Counter(words)
-    sorted_vocab: List[int] = sorted(word_counts, key=word_counts.get, reverse=True)
+    vocab_words = [w for w, f in word_counts.items() if f >= min_freq]
 
-    int_to_vocab: Dict[int, str] = {ii: word for ii, word in enumerate(sorted_vocab)}
-    vocab_to_int: Dict[str, int] = {word: ii for ii, word in int_to_vocab.items()}
+    vocab_complete = special_tokens + vocab_words
+    word2idx = {vocab_complete[i]: i for i in range(len(vocab_complete))}
+    idx2word = {i: w for w, i in word2idx.items()}
+    
+    return word2idx, idx2word
 
-    return vocab_to_int, int_to_vocab
+def text_to_indices(tokens: List[str], word2idx: Dict[str,int], max_len=None) -> List[int]:
+    indices = [word2idx.get(tok, word2idx['<UNK>']) for tok in tokens]
+
+    if max_len:
+        indices = indices[:max_len] + [word2idx['<PAD>']] * max(0, max_len - len(indices))
+
+    return indices
+
+def create_embedding_matrix(word2idx: Dict[str,int], embedding_dim=100) -> np.ndarray:
+    vocab_size = len(word2idx)
+    embedding_matrix = np.random.normal(scale=0.6, size=(vocab_size, embedding_dim)).astype(np.float32)
+
+    embedding_matrix[word2idx['<PAD>']] = np.zeros(embedding_dim, dtype=np.float32)
+    return embedding_matrix
+
+
+def load_processed_data_with_embeddings(infile: str, min_freq=2, max_len=None, embedding_dim=100):
+    # Devuelve los textos de input X en forma de lista de índices, y los outpus Y en forma de 0 (negativo) y 1 (positivo)
+    # Devuelve los diccionarios de conversión palabra - índice
+    # Devuelve la matriz de embeddings, ya sea preentrenada o por entrenar
+
+    tokens_list, labels_list = get_tokens_and_labels(infile)
+    word2idx, idx2word = create_lookup_tables(tokens_list, min_freq=min_freq)
+
+    X = [text_to_indices(tokens, word2idx, max_len=max_len) for tokens in tokens_list]
+    y = labels_list
+    embedding_matrix = create_embedding_matrix(word2idx, embedding_dim=embedding_dim)
+    
+    return np.array(X), np.array(y), word2idx, idx2word, embedding_matrix
--- src/main.py
+++ src/main.py
@@ -1,11 +1,23 @@
-from src.data_processing import load_and_preprocess_data
+from src.data_processing import load_processed_data_with_embeddings
+import torch 
 
 def main():
-    data = load_and_preprocess_data("data/imdb.csv")
-    
-    print("Número de ejemplos:", len(data))
-    print("Tokens:", data[0][0])
-    print("Label:", data[0][1])
+    data_dir = "data/imdb.csv"
+
+    X, y, word2idx, idx2word, embedding_matrix = load_processed_data_with_embeddings(
+        data_dir, min_freq=2, max_len=100, embedding_dim=100
+    )
+
+    X_tensor = torch.tensor(X, dtype=torch.long)
+    y_tensor = torch.tensor(y, dtype=torch.float32)
+    print(X_tensor[0])
+    print(y_tensor[0])
+
+    embedding_layer = torch.nn.Embedding.from_pretrained(
+        torch.tensor(embedding_matrix, dtype=torch.float32),
+        freeze=False 
+    )
+
 
 if __name__ == "__main__":
     main()
